2f757b362d78948e5737812bb32e3a566b3af02d
Gio-log <gierimaniak@gmail.com>
2026-04-27 12:49:24 +0200

    Init

---
+++ Backup_maker/Form1.cs
@@ -0,0 +1,10 @@
+namespace Backup_maker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align="left">Backup maker</h1>
+A simple App to automaticaly make backups of files made in C#.
