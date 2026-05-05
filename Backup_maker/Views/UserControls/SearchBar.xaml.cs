d9c9a03b3e2b776d5f24574786fdc053348f1eca
Gio-log <gierimaniak@gmail.com>
2026-05-05 16:26:46 +0200

    Deleted obsolete label in MainWindow and made placeholder text in searchbar customizable for each one separately

--- Backup_maker/Views/UserControls/SearchBar.xaml.cs
+++ Backup_maker/Views/UserControls/SearchBar.xaml.cs
@@ -6,6 +6,13 @@ namespace Backup_Maker.Views.UserControls
 {
     public partial class SearchBar : UserControl
     {
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register(
+                nameof(Placeholder),
+                typeof(string),
+                typeof(SearchBar),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
         public static readonly DependencyProperty SearchFileProperty =
             DependencyProperty.Register(
                 nameof(SearchFile),
@@ -21,6 +28,11 @@ namespace Backup_Maker.Views.UserControls
         public SearchBar()
         {
             InitializeComponent();
+        }
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
         }
 
         private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
