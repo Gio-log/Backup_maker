225ccb6983775446ed46843b21ad8085940461f8
Gio-log <gierimaniak@gmail.com>
2026-05-05 15:53:52 +0200

    Changed FileList control to make it usable for displaying both Files to backup and already made backups.

--- Backup_maker/Views/UserControls/FileList.xaml.cs
+++ Backup_maker/Views/UserControls/FileList.xaml.cs
@@ -1,13 +1,60 @@
 ﻿using Backup_Maker.ViewModels;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Backup_Maker.Views.UserControls
 {
-    /// <summary>
-    /// Logika interakcji dla klasy FileList.xaml
-    /// </summary>
     public partial class FileList : UserControl
     {
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(FileList),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(
+                nameof(ItemsSource),
+                typeof(IEnumerable),
+                typeof(FileList),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(
+                nameof(SelectedItem),
+                typeof(object),
+                typeof(FileList),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register(
+                nameof(SelectedItems),
+                typeof(IList),
+                typeof(FileList),
+                new FrameworkPropertyMetadata(null));
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+        public object SelectedItem
+        {
+            get => (object)GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+        public IList SelectedItems
+        {
+            get => (IList)GetValue(SelectedItemsProperty);
+            set => SetValue(SelectedItemsProperty, value);
+        }
         public FileList()
         {
             InitializeComponent();
