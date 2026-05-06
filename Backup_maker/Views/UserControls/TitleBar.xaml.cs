4b6efbe4d5d9ebe4cbfd232682b468cfb064c715
Gio-log <gierimaniak@gmail.com>
2026-05-06 13:27:53 +0200

    Added Custom TitleBar

---
+++ Backup_maker/Views/UserControls/TitleBar.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Backup_Maker.Views.UserControls
+{
+    public partial class TitleBar : UserControl
+    {
+        private Window ParentWindow => Window.GetWindow(this);
+        public TitleBar()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => ParentWindow.DragMove();
+
+        private void Minimize_Click(object sender, RoutedEventArgs e) => ParentWindow.WindowState = WindowState.Minimized;
+
+        private void Maximize_Click(object sender, RoutedEventArgs e) => ParentWindow.WindowState = ParentWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+        private void Close_Click(object sender, RoutedEventArgs e) => ParentWindow.Close();
+    }
+}
