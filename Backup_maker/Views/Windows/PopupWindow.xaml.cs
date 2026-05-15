3bece5e25b6aeb690be512f2290f5f8cf53c1e17
Gio-log <gierimaniak@gmail.com>
2026-05-15 08:40:15 +0200

    Fixed PopupWindow

--- Backup_maker/Views/Windows/PopupWindow.xaml.cs
+++ Backup_maker/Views/Windows/PopupWindow.xaml.cs
@@ -3,28 +3,35 @@ using System.Windows.Controls;
 
 namespace Backup_Maker.Views.Windows
 {
+    public enum PopupButtons
+    {
+        YesNo,
+        OK
+    }
+
     public partial class PopupWindow : Window
     {
-        private string Title;
-        private string Message;
-        public PopupWindow(string title, string message)
+        public string WindowTitle { get; set; }
+        public string Message { get; set; }
+
+
+        public PopupWindow(string title, string message, PopupButtons buttons)
         {
             InitializeComponent();
-            Title = title;
+            DataContext = this;
+            WindowTitle = title;
             Message = message;
-        }
 
-        public void YesNo()
-        {
-            Yes.Visibility = Visibility.Visible;
-            No.Visibility = Visibility.Visible;
+            if (buttons == PopupButtons.YesNo)
+            {
+                Yes.Visibility = Visibility.Visible;
+                No.Visibility = Visibility.Visible;
+            }
+            else if (buttons == PopupButtons.OK)
+            {
+                Ok.Visibility = Visibility.Visible;
+            }
         }
-
-        public void OK()
-        {
-            Ok.Visibility = Visibility.Visible;
-        }
-
         private void Yes_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
